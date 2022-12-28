f809ca10e84f495069acd96b7eb039e1e6bd8c15
KhaledAlabssi <khaled.absi5@gmail.com>
2022-12-28 13:50:24 +0100

    finalize simple and api server

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import {products} from './data.js'
 
 const app = express();
 
@@ -8,6 +9,40 @@ app.get('/', (req, res) => {
 
 app.get('/about', (req, res) => {
     res.status(200).send("About page")
+})
+
+app.get('/products',(req, res) => {
+    // res.json(products)
+    let result = products.map(i => {
+        const {id, name, price, image} = i;
+        return {id, name, price, image}
+    })
+
+    res.json(result)
+})
+
+app.get('/products/:id', (req, res) => {
+    console.log(req.params);
+    let {id} = req.params;
+    let result = products.find(i => i.id == id)
+    console.log(result);
+    if(!result) {
+        return res.status(404).send("Product not found!")
+    }
+    res.json(result)
+})
+
+app.get('/api/v1/query', (req, res) => {
+    const {search, limit} = req.query;
+    let sortedProducts = [...products];
+    if(search) {
+        sortedProducts = sortedProducts.filter(i => i.name.includes(search))
+    }
+    if(limit) {
+        sortedProducts = sortedProducts.slice(0, limit)
+    }
+
+    res.status(200).json(sortedProducts)
 })
 
 app.all("*", (req, res) => {
